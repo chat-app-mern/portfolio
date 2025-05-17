87a04122b7b4f614bd8bdf992d30c19ebdf8e7f7
mauryasoni2201 <maurya@krishaweb.com>
2025-05-17 16:39:55 +0530

    Solved issue.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './assets/css/bootstrap.min.css';
 import './assets/css/style.css';
 import './assets/css/media.css';
 
