5a24054e49cd6bcc841eeb86eea25154fd12b323
mauryasoni2201 <maurya@krishaweb.com>
2025-12-02 14:46:04 +0530

    Remove code files.

--- src/components/SkillCard.jsx
+++
@@ -1,8 +0,0 @@
-const SkillCard = ({image,alt}) => {
-  return (
-    <div data-aos-delay="400" data-aos="fade-up" className='skill'>
-    <img alt={alt} src={image} loading="lazy" height={'42'} width={'42'} />
-    </div>
-  )
-}
-export default SkillCard;
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './assets/css/style.css';
-import './assets/css/media.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
